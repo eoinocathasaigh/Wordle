b50f794542c9aee3f849fb9ee3f8cae724b55441
eoinocathasaigh <G00417466@atu.ie>
2023-12-22 14:15:48 +0000

    Created the sign in page and added some more images to it, also added the toolkit to this project

--- Wordle/SignIn.xaml.cs
+++ Wordle/SignIn.xaml.cs
@@ -2,6 +2,10 @@ namespace Wordle;
 
 public partial class SignIn : ContentPage
 {
+	//Initializing variables for this particular class and the others that may need them
+	public string userName;
+	private string password;
+
 	public SignIn()
 	{
 		InitializeComponent();
