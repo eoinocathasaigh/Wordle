c7477a06e5e9f03a19d689383879e5160cbc2d4e
eoinocathasaigh <G00417466@atu.ie>
2023-12-22 14:21:02 +0000

    Setting the users name and password depending on the entrys

--- Wordle/SignIn.xaml.cs
+++ Wordle/SignIn.xaml.cs
@@ -4,7 +4,7 @@ public partial class SignIn : ContentPage
 {
 	//Initializing variables for this particular class and the others that may need them
 	public string userName;
-	private string password;
+	private string userPass;
 
 	public SignIn()
 	{
@@ -13,6 +13,7 @@ public partial class SignIn : ContentPage
 
     private void Save_Clicked(object sender, EventArgs e)
     {
-
+		userName = name.Text;
+		userPass = password.Text;
     }
 }
